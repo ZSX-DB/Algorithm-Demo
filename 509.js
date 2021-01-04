6178d4ba3fe32c5d4cfad24b12039e0fa63f7967
ZSX-DB <Sx82199609124535@outlook.com>
2021-01-05 00:03:39 +0800

    update

--- 509.js
+++ 509.js
@@ -4,10 +4,28 @@
  * 链接：https://leetcode-cn.com/problems/fibonacci-number/
  */
 
+// const fib = n => {
+//     if (n === 0) return 0
+//     if (n === 1) return 1
+//     return fib(n - 1) + fib(n - 2)
+// }
+
+// const fib = n => {
+//     const list = [0, 1]
+//     if(n>=2){
+//         for(let i=2;i<=n;i++){
+//             list[i]= list[i-1]+list[i-2]
+//         }
+//     }
+//     return list[n]
+// }
+
+// 通项公式(但存在浮点问题)
 const fib = n => {
-    if (n === 0) return 0
-    if (n === 1) return 1
-    return fib(n - 1) + fib(n - 2)
+    const sqrt5 = Math.sqrt(5)
+    const fibN = Math.pow((1 + sqrt5) / 2, n) - Math.pow((1 - sqrt5) / 2, n)
+    return Math.round(fibN / sqrt5)
 }
 
-console.log(fib(2), fib(4), fib(3))
+
+console.log(fib(4))
