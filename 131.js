b8969a3490e80fc78b6f4046e66f7f93f404bfba
ZSX-DB <Sx82199609124535@outlook.com>
2021-03-09 00:23:56 +0800

    update

--- 131.js
+++ 131.js
@@ -1,53 +1,15 @@
 // 回溯
-// const partition = s => {
-//     const len = s.length
-//     const res = []
-//
-//     // 双指针，判断是否是回文
-//     const judge = (str, left, right) => {
-//         while (left < right) {
-//             if (str[left] !== str[right]) return false
-//             left++
-//             right--
-//         }
-//         return true
-//     }
-//
-//     const dfs = (tmp, start) => {
-//         if (start === len) {
-//             res.push(tmp.slice())
-//             return
-//         }
-//         for (let i = start; i < len; i++) {
-//             if (judge(s, start, i)) {
-//                 tmp.push(s.substring(start, i + 1))
-//                 dfs(tmp, i + 1)
-//                 tmp.pop()
-//             }
-//         }
-//     }
-//
-//     dfs([], 0)
-//     return res
-// }
-
-// 优化，加入记忆化搜索
 const partition = s => {
     const len = s.length
-    const used = {}
     const res = []
 
+    // 双指针，判断是否是回文
     const judge = (str, left, right) => {
-        if (used[`${left}${right}`] !== undefined) return used[`${left}${right}`]
         while (left < right) {
-            if (str[left] !== str[right]) {
-                used[`${left}${right}`] = false
-                return false
-            }
+            if (str[left] !== str[right]) return false
             left++
             right--
         }
-        used[`${left}${right}`] = true
         return true
     }
 
