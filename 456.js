61ac74e022b202157205979bb075390431736091
ZSX-DB <Sx82199609124535@outlook.com>
2021-03-24 23:54:55 +0800

    update

--- 456.js
+++ 456.js
@@ -1,12 +1,3 @@
-// const find132pattern = nums => {
-//     const len = nums.length
-//     if (len < 3) return false
-//     for (let i = 2; i < len; i++) {
-//         if (nums[i - 2] < nums[i] && nums[i] < nums[i - 1]) return true
-//     }
-//     return false
-// }
-
 const find132pattern = nums => {
     const len = nums.length
     if (len < 3) return false
@@ -14,13 +5,18 @@ const find132pattern = nums => {
     while (minIdx < len) {
         const stack = [nums[minIdx]]
         for (let i = minIdx + 1; i < len; i++) {
-            // if()
+            if (nums[i] > stack[0] && stack.length === 1) stack.push(nums[i])
+            if (nums[i] > stack[1] && stack.length === 2) stack[1] = nums[i]
+            if (nums[i] > stack[0] && nums[i] < stack[1]) return true
         }
+        minIdx++
     }
+    return false
 }
 
-// console.log(find132pattern([1, 2, 3, 4]))
-// console.log(find132pattern([3, 1, 4, 2]))
-// console.log(find132pattern([-1, 3, 2, 0]))
-// [3,5,0,3,4]
+
+console.log(find132pattern([1, 2, 3, 4]))
+console.log(find132pattern([3, 1, 4, 2]))
+console.log(find132pattern([-1, 3, 2, 0]))
 console.log(find132pattern([3, 5, 0, 3, 4]))
+console.log(find132pattern([-2, 1, 2, -2, 1, 2]))
