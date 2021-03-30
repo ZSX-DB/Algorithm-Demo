3e3ca1bf310b39db8841aa43402e2aa7340e6d69
ZSX-DB <Sx82199609124535@outlook.com>
2021-03-30 23:47:37 +0800

    update

--- 二叉树/demo.js
+++ 二叉树/demo.js
@@ -20,28 +20,95 @@ const binaryTree = {
     }
 }
 
-// 按中序输出节点
-const inorderLog = root => {
-    if(root === null) return
-    inorderLog(root.left)
-    console.log(root.val)
-    inorderLog(root.right)
-}
+// const binaryTree = {
+//     val: 7,
+//     left: {
+//         val: 3,
+//         left: {
+//             val: 4,
+//             left: null,
+//             right: null
+//         },
+//         right: {
+//             val: 8,
+//             left: null,
+//             right: null
+//         }
+//     },
+//     right: {
+//         val: 15,
+//         left: {
+//             val: 9,
+//             left: null,
+//             right: null
+//         },
+//         right: {
+//             val: 20,
+//             left: null,
+//             right: null
+//         }
+//     }
+// }
 
-// 获取中序遍历节点
-const inorderTraversal = root => {
+/**
+ * 递归方式遍历二叉树
+ * @param root
+ * @returns {[]}
+ */
+
+// 前序遍历
+const preorderTraversal = root => {
     const list = []
-    const fn = root => {
-        if(root === null) return
-        fn(root.left)
+    const iterate = root => {
+        if (root === null) return
         list.push(root.val)
-        fn(root.right)
+        iterate(root.left)
+        iterate(root.right)
     }
-
-    fn(root)
+    iterate(root)
     return list
 }
 
+// 中序遍历
+const inorderTraversal = root => {
+    const list = []
+    const iterate = root => {
+        if (root === null) return
+        iterate(root.left)
+        list.push(root.val)
+        iterate(root.right)
+    }
+    iterate(root)
+    return list
+}
 
-// inorderLog(binaryTree)
-console.log(inorderTraversal(binaryTree))
+// 后序遍历
+const postorderTraversal = root => {
+    const list = []
+    const iterate = root => {
+        if (root === null) return
+        iterate(root.left)
+        iterate(root.right)
+        list.push(root.val)
+    }
+    iterate(root)
+    return list
+}
+
+console.log('前序遍历', preorderTraversal(binaryTree))
+console.log('中序遍历', inorderTraversal(binaryTree))
+console.log('后序遍历', postorderTraversal(binaryTree))
+
+const f1 = root => {
+    const stack = [root]
+    const list = []
+    while (stack.length) {
+        const cur = stack.pop()
+        list.push(cur.val)
+        if (cur.right) stack.push(cur.right)
+        if (cur.left) stack.push(cur.left)
+    }
+    return list
+}
+
+console.log(f1(binaryTree))
