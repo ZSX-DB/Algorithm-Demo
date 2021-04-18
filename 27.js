bdc33a7158ce515f911bd31e96df2cfb45bf4f47
ZSX-DB <Sx82199609124535@outlook.com>
2021-04-19 00:50:57 +0800

    update

--- 27.js
+++ 27.js
@@ -6,21 +6,28 @@
  * 链接：https://leetcode-cn.com/problems/remove-element
  */
 
+// const removeElement = (nums, val) => {
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] === val) {
+//             nums.splice(i, 1)
+//             i--
+//         }
+//     }
+//     return nums.length
+// }
 
-function removeElement (nums, val) {
-    let len=nums.length,
-        i=0
-    for(let j=0;j<len;j++){
-        if(val!==nums[j]){
-            nums[i]=nums[j]
+const removeElement = (nums, val) => {
+    let i = 0
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] !== val) {
+            nums[i] = nums[j]
             i++
         }
     }
     return i
-
 }
 
-console.log(removeElement([0,1,2,2,3,0,4,2],2))
-console.log(removeElement([3,2,2,3],3))
-console.log(removeElement([0,0,0,1],0))
-console.log(removeElement([],0))
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
+console.log(removeElement([3, 2, 2, 3], 3))
+console.log(removeElement([0, 0, 0, 1], 0))
+console.log(removeElement([], 0))
