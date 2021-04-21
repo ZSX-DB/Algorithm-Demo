c44c3e25b4f81a08b974ade97bf7faa54054ddee
ZSX-DB <Sx82199609124535@outlook.com>
2021-04-22 02:46:35 +0800

    update

--- 91.js
+++ 91.js
@@ -1,14 +1,12 @@
 const numDecodings = s => {
+    if (s[0] === '0') return 0
     const len = s.length
-    s = s.replace(/^0+/, '')
-    if (len !== s.length) return 0
     if (len === 1) return 1
     if (Number(s[0]) > 2 && s[1] === '0') return 0
-    const dp = new Array(len + 1).fill(0)
+    const dp = new Array(len).fill(0)
     const num = Number(`${s[0]}${s[1]}`)
     dp[0] = 1
     dp[1] = (num > 26 || num === 10 || num === 20) ? 1 : 2
-
     for (let i = 2; i < len; i++) {
         if (s[i] !== '0') dp[i] += dp[i - 1]
         if (s[i - 1] !== '0' && Number(`${s[i - 1]}${s[i]}`) <= 26) dp[i] += dp[i - 2]
@@ -16,82 +14,16 @@ const numDecodings = s => {
     return dp[len - 1]
 }
 
-// 1 2 2
-// 12 2
-// 1 22
-// 1221
+// 以 1221 为例
+// 第一种情况：在 s[3] 时，可以是 122 加 1，即 122 的总数 3
+// 第二种情况：也可以是 12 加 21，即 12 的总数 2
+// 两种情况不会重叠，第一种情况选了 12 2 不可能添加 1 出现 12 21 的可能性，故为 5
 
 
-// var numDecodings = function(s) {
-//     // 处理前导 0
-//     s = s.replace(/^0+/, "");
-//     if (s.length === 0) return 0;
-//
-//
-//     let list = [];
-//     // console.log(s.indexOf("0"), s.substring(0, 2), s.substring(2, 4), s.substring(4));
-//     while (s.includes("0")) {
-//         const idx = s.indexOf("0");
-//         list.push(s.substring(0, idx - 1), s.substring(idx - 1, idx + 1));
-//         s = s.substring(idx + 1);
-//     }
-//     list.push(s);
-//     list = list.filter(item => item !== "" && item !== "0");
-//     // return list;
-//     const getNum = str => {
-//         if (str.length === 1) return 1;
-//         if (str.length === 2) {
-//             if (str[1] === "0") return 1;
-//             if (Number(str) > 26) return 1;
-//             // if (Number(str) > 26) return 0;
-//             return 2;
-//         }
-//     };
-//
-//     return list.map(item => {
-//         // if (item.length === 1) return 1;
-//         // if (item.length === 2) {
-//         //   if (item[1] === "0") return 1;
-//         //   if (Number(item) > 26) return 1;
-//         //   return 2;
-//         // }
-//         if (item.length <= 2) return getNum(item);
-//         // const dp = [getNum(item[0]), getNum(`${item[0]}${item[1]}`)];
-//         const dp = new Array(item.length).fill(0);
-//         dp[0] = getNum(item[0]);
-//         dp[1] = getNum(`${item[0]}${item[1]}`);
-//         for (let i = 2; i < item.length; i++) {
-//             if(Number(`${s[i-1]}${s[i]}`)> 26){
-//                 dp[i] = dp[i-1]
-//             }
-//             // dp[i] = Math.max(dp[i - 1] * getNum(item.substring(i, i + 1)), dp[i - 2] * getNum(item.substring(i - 1, i + 1)));
-//             dp[i] = Math.max(dp[i - 1] + getNum(s[i]), dp[i - 2] + getNum(`${s[i-1]}${s[i]}`));
-//
-//         }
-//         console.log("dp", dp);
-//         return dp[dp.length - 1];
-//     });
-//
-// };
-
-// var numDecodings = function(s) {
-//     const n = s.length;
-//     const f = new Array(n + 1).fill(0);
-//     f[0] = 1;
-//     for (let i = 1; i <= n; ++i) {
-//         if (s[i - 1] !== '0') {
-//             f[i] += f[i - 1];
-//         }
-//         if (i > 1 && s[i - 2] != '0' && ((s[i - 2] - '0') * 10 + (s[i - 1] - '0') <= 26)) {
-//             f[i] += f[i - 2];
-//         }
-//     }
-//     console.log(f)
-//     return f[n];
-// };
-
-console.log(numDecodings('11106'))
-console.log(numDecodings('226'))
 console.log(numDecodings('1221'))
-console.log(numDecodings('2101'))
+console.log(numDecodings('1231'))
+// console.log(numDecodings('2101'))
+// console.log(numDecodings('11106'))
+// console.log(numDecodings('226'))
+// console.log(numDecodings('360'))
 
