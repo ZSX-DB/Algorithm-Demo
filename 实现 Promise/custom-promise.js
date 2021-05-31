2775a7531c46d867d8da9a52c025d0f0533bfd24
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2021-06-01 01:52:17 +0800

    update

--- 实现 Promise/custom-promise.js
+++ 实现 Promise/custom-promise.js
@@ -3,134 +3,262 @@ const FULFILLED = 1
 const REJECTED = 2
 
 
-/**
- * 检查一个值是否为 Promise , 若为 Promise 则返回该 Promise 的 then
- * @param {Promise|all} value
- * @return {Function|null}
- */
-const getThen = value => {
-    const type = typeof value
-    if (value && (type === 'object' || type === 'function')) {
-        const then = value.then
-        if (typeof then === 'function') return then
-    }
-    return null
-}
-
-const doResolve = (fn, onFulfilled, onRejected) => {
-    let done = false
-    try {
-        fn(value => {
-            if (done) return
-            done = true
-            onFulfilled(value)
-        }, reason => {
-            if (done) return
-            done = true
-            onRejected(reason)
-        })
-    } catch (error) {
-        if (done) return
-        done = true
-        onRejected(error)
+const resolvePromise = (promiseCopy, res, resolve, reject) => {
+    // 如果相同则为循环引用，返回 TypeError
+    if (promiseCopy === res) return reject(new TypeError('Chaining cycle detected for promise #<Promise>'))
+    // 判断是否为 CustomPromise 实例
+    if (res instanceof CustomPromise) {
+        // 调用 then，目的是改变状态为 fulfilled 或 rejected
+        res.then(resolve, reject)
+    } else {
+        resolve(res)
     }
 }
 
 class CustomPromise {
-    constructor(fn) {
-        // 存储该 CustomPromise 的状态信息
-        let state = PENDING
-        // 存储 FULFILLED 或 REJECTED 时带来的数据
-        let value = null
-        // 存储 then 或 done 时调用的成功或失败回调
-        let handlers = []
-
-        const fulfill = result => {
-            state = FULFILLED
-            value = result
+    constructor(executor) {
+        // bind 绑定避免 this 丢失
+        this.resolve = this.resolve.bind(this)
+        this.reject = this.reject.bind(this)
+        // 初始状态
+        this.state = PENDING
+        // 存储数据
+        this.value = null
+        this.reason = null
+        // 存储成功或失败回调函数，用于处理异步或 setTimeout 等宏任务
+        this.onFulfilledCallback = []
+        this.onRejectedCallback = []
+        // 执行器，默认执行，需要放置于最后让数据初始化完毕，try catch 添加错误处理
+        try {
+            executor(this.resolve, this.reject)
+        } catch (error) {
+            this.reject(error)
         }
+    }
 
-        const reject = error => {
-            state = REJECTED
-            value = error
-        }
-
-        const resolve = (result) => {
-            try {
-                const then = getThen(result)
-                if (then) {
-                    doResolve(then.bind(result), resolve, reject)
-                    return
-                }
-                fulfill(result)
-            } catch (error) {
-                reject(error)
+    resolve(value) {
+        if (this.state === PENDING) {
+            this.state = FULFILLED
+            this.value = value
+            // 判断成功回调是否存在，如果存在就调用
+            while (this.onFulfilledCallback.length) {
+                this.onFulfilledCallback.shift()(value)
             }
         }
+    }
 
-        // 保证 done 中回调的执行
-        const handle = handler => {
-            if (state === PENDING) {
-                handlers.push(handler)
-            } else {
-                if (state === FULFILLED && typeof handler.onFulfilled === 'function') {
-                    handler.onFulfilled(value)
-                }
-                if (state === REJECTED && typeof handler.onRejected === 'function') {
-                    handler.onRejected(value)
-                }
+    reject(reason) {
+        if (this.state === PENDING) {
+            this.state = REJECTED
+            this.reason = reason
+            // 判断失败回调是否存在，如果存在就调用
+            while (this.onRejectedCallback.length) {
+                this.onRejectedCallback.shift()(reason)
             }
         }
+    }
 
-        this.done = (onFulfilled, onRejected) => {
-            // 保证 done 总是异步执行
-            setTimeout(() => {
-                handle({
-                    onFulfilled,
-                    onRejected
-                })
-            }, 0)
+    then(onFulfilled, onRejected) {
+
+        // 实现参数可选
+        onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value
+        onRejected = typeof onRejected === 'function' ? onRejected : error => {
+            throw error
         }
 
-        this.then = (onFulfilled, onRejected) => {
-            const _this = this
-            return new CustomPromise((resolve, reject) => _this.done(
-                result => {
-                    if (typeof onFulfilled === 'function') {
+        const promiseCopy = new CustomPromise((resolve, reject) => {
+            switch (this.state) {
+                case FULFILLED:
+                    queueMicrotask(() => {
                         try {
-                            return resolve(onFulfilled(result))
+                            const res = onFulfilled(this.value)
+                            resolvePromise(promiseCopy, res, resolve, reject)
                         } catch (error) {
-                            return reject(error)
+                            reject(error)
                         }
-                    } else {
-                        return resolve(result)
-                    }
-                }, error => {
-                    if (typeof onRejected === 'function') {
+                    })
+                    break
+                case REJECTED:
+                    queueMicrotask(() => {
                         try {
-                            return resolve(onRejected(error))
+                            const res = onRejected(this.reason)
+                            resolvePromise(promiseCopy, res, resolve, reject)
                         } catch (error) {
-                            return reject(error)
+                            reject(error)
                         }
-                    } else {
-                        return reject(error)
-                    }
-                }))
-        }
+                    })
+                    break
+                case PENDING:
+                    this.onFulfilledCallback.push(() => {
+                        queueMicrotask(() => {
+                            try {
+                                const res = onFulfilled(this.value)
+                                resolvePromise(promiseCopy, res, resolve, reject)
+                            } catch (error) {
+                                reject(error)
+                            }
+                        })
+                    })
+                    this.onRejectedCallback.push(() => {
+                        queueMicrotask(() => {
+                            try {
+                                const res = onRejected(this.reason)
+                                resolvePromise(promiseCopy, res, resolve, reject)
+                            } catch (error) {
+                                reject(error)
+                            }
+                        })
+                    })
+                    break
+            }
+        })
+        return promiseCopy
+    }
 
-        doResolve(fn, resolve, reject)
+    catch(onRejected) {
+        return this.then(null, onRejected)
+    }
+
+    static resolve(parameter) {
+        if (parameter instanceof CustomPromise) return parameter
+        return new CustomPromise(resolve => {
+            resolve(parameter)
+        })
+    }
+
+    static reject(reason) {
+        return new CustomPromise((_, reject) => {
+            reject(reason)
+        })
     }
 
 }
 
-new CustomPromise(resolve => {
-    resolve(223)
-}).then(val => {
-    console.log(val)
-    return val + 5
-}).then(val => {
-    console.log(val)
-    return val - 2
-}).then(val => {
-    console.log(val)
+// -------------------------------------------------------------    测试链式调用 No. 1
+
+// const promise = new CustomPromise((resolve, reject) => {
+//     resolve('success')
+// })
+
+// const other = () => new CustomPromise((resolve, reject) => {
+//     resolve('other')
+// })
+
+// promise.then(value => {
+//     console.log(1)
+//     console.log('resolve', value)
+//     return other()
+// }).then(value => {
+//     console.log(2)
+//     console.log('resolve', value)
+// })
+
+// -------------------------------------------------------------    测试链式调用 No. 2
+
+// new CustomPromise(resolve => {
+//     resolve(223)
+// }).then(val => {
+//     console.log(val)
+//     return val + 5
+// }).then(val => {
+//     console.log(val)
+//     return val - 2
+// }).then(val => {
+//     console.log(val)
+//     return val + 74
+// })
+
+// -------------------------------------------------------------    测试循环引用
+
+// const promise = new CustomPromise((resolve, reject) => {
+//     resolve(100)
+// })
+// const p1 = promise.then(value => {
+//     console.log(value)
+//     return p1
+// })
+// p1.then(value => {
+//     console.log(2)
+//     console.log('resolve', value)
+// }, reason => {
+//     console.log(3)
+//     console.log(reason.message)
+// })
+
+// -------------------------------------------------------------    测试捕捉执行器错误
+
+// new CustomPromise((resolve, reject) => {
+//     throw new Error('Executor Error')
+// }).then(val => {
+//     console.log(val)
+// }, reason => {
+//     console.log(reason.message)
+// })
+
+// -------------------------------------------------------------    测试 then 执行错误
+
+// new CustomPromise(resolve => {
+//     resolve(333)
+// }).then(val => {
+//     console.log(val)
+//     // 一个未定义的变量
+//     console.log(un)
+// }).then(val => {
+//     console.log(1)
+//     console.log(val)
+// }, reason => {
+//     console.log(2)
+//     console.log(reason.message)
+// })
+
+// -------------------------------------------------------------    测试参数可选
+
+// new CustomPromise(resolve => {
+//     resolve(223)
+// })
+//     .then(val => {
+//         console.log(val)
+//         return val + 5
+//     })
+//     .then(val => {
+//         console.log(val)
+//         return val - 2
+//     })
+//     .then(val => {
+//         console.log(val)
+//         return val + 74
+//     })
+//     .then()
+//     .then()
+//     .then(val => {
+//         console.log('last', val)
+//     })
+
+// -------------------------------------------------------------    测试静态属性
+
+// CustomPromise.resolve().then(() => {
+//     console.log(0)
+//     return CustomPromise.resolve(4)
+// }).then(val => {
+//     console.log(val)
+// })
+
+// -------------------------------------------------------------    测试 catch
+
+const cp = new CustomPromise((_, reject) => {
+    reject('fail')
+})
+
+cp.catch(reason => {
+    console.log('reason', reason)
+}).then(() => {
+    console.log('middle')
+    return 10
+}).then(value => {
+    console.log(value)
+}).then(() => {
+    console.log('end')
+    throw Error('End error')
+}).catch(reason => {
+    console.log(reason.message)
 })
