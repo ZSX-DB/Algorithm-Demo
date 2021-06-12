595343f9a3f24140b4a3fdc72d8bfa282d1f18b8
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2021-06-12 16:39:48 +0800

    update

--- 堆/min-heap.js
+++ 堆/min-heap.js
@@ -4,40 +4,107 @@
  * e.g. 下标为 i 的节点，其父节点下标为 Math.floor((i - 1) / 2) 或 Math.ceil(i / 2) - 1 ，其左右子节点的下标分别为 2 * i + 1 和 2 * i + 2
  */
 
+// class MinHeap {
+//     constructor() {
+//         this.heap = []
+//     }
+//
+//     swap(i, j) {
+//         [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]]
+//     }
+//
+//     up(idx) {
+//         let parentIdx = Math.floor((idx - 1) / 2)
+//         if (this.heap[idx] < this.heap[parentIdx]) {
+//             this.swap(idx, parentIdx)
+//             this.up(parentIdx)
+//         }
+//     }
+//
+//     down(idx) {
+//         let to = idx
+//         let left = idx * 2 + 1
+//         let right = idx * 2 + 2
+//         if (this.heap[to] > this.heap[left]) to = left
+//         if (this.heap[to] > this.heap[right]) to = right
+//         if (to !== idx) {
+//             this.swap(idx, to)
+//             this.down(to)
+//         }
+//     }
+//
+//     add(value) {
+//         this.heap.push(value)
+//         this.up(this.heap.length - 1)
+//     }
+//
+//     init(nums) {
+//         for (const num of nums) {
+//             this.add(num)
+//         }
+//     }
+//
+//     clear() {
+//         this.heap = []
+//     }
+//
+// }
+
 class MinHeap {
     constructor() {
-        this.minHeap = [-Infinity]
+        this.heap = []
     }
 
     swap(i, j) {
-        [this.minHeap[i], this.minHeap[j]] = [this.minHeap[j], this.minHeap[i]]
+        [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]]
     }
 
-    up(idx) {
-        let parentIdx = Math.floor((idx - 1) / 2)
-        if (this.minHeap[idx] < this.minHeap[parentIdx]) {
-            this.swap(idx, parentIdx)
-            this.up(parentIdx)
+    size() {
+        return this.heap.length
+    }
+
+    // up(idx) {
+    //     let parentIdx = Math.floor((idx - 1) / 2)
+    //     if (this.heap[idx] < this.heap[parentIdx]) {
+    //         this.swap(idx, parentIdx)
+    //         this.up(parentIdx)
+    //     }
+    // }
+
+    build() {
+        for (let i = Math.floor(this.size() / 2) - 1; i >= 0; i--) {
+            this.heapify(i)
         }
     }
 
-    down(idx) {
+    heapify(idx) {
         let to = idx
         let left = idx * 2 + 1
         let right = idx * 2 + 2
-        if (this.minHeap[to] > this.minHeap[left]) to = left
-        if (this.minHeap[to] > this.minHeap[right]) to = right
+        if (this.heap[to] > this.heap[left]) to = left
+        if (this.heap[to] > this.heap[right]) to = right
         if (to !== idx) {
             this.swap(idx, to)
-            this.down(to)
+            this.heapify(to)
         }
     }
 
     add(value) {
-        this.minHeap.push(value)
-        this.up(this.minHeap.length - 1)
+        this.heap.push(value)
+        this.build()
+    }
+
+    init(nums) {
+        for (const num of nums) {
+            this.add(num)
+        }
+    }
+
+    clear() {
+        this.heap = []
     }
 
 }
+
 
 module.exports = MinHeap
