8b27a9b03c0b6a05d8513b53eab1ddb9e1f4d1a8
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2021-06-18 16:15:17 +0800

    update

--- 470.js
+++ 470.js
@@ -1,22 +1,30 @@
 const rand7 = () => Math.ceil(Math.random() * 7)
 
-// flag === 1 和 2 的概率是一样的
-const createRand10 = () => {
-    let flag
-    while (true) {
-        const num = rand7()
-        if (num !== 7) {
-            flag = num <= 3 ? 1 : 2
-            break
-        }
+// const rand10 = () => {
+//     let flag = 7
+//     while (flag === 7) {
+//         flag = rand7()
+//     }
+//     while (true) {
+//         const num = rand7()
+//         if (num <= 5) return flag <= 3 ? num : 5 + num
+//     }
+// }
+
+// 官方解法
+const rand10 = () => {
+    let row
+    let col
+    let flag = 41
+    while (flag > 40) {
+        row = rand7()
+        col = rand7()
+        flag = row + (col - 1) * 7
     }
-    while (true) {
-        const num = rand7()
-        if (num <= 5) return flag === 1 ? num : 5 + num
-    }
+    return 1 + (flag - 1) % 10
 }
 
-const rand10 = n => new Array(n).fill(0).map(_ => createRand10())
+const createRand10List = n => new Array(n).fill(0).map(_ => rand10())
 
 const verify = nums => {
     const counts = new Array(11).fill(0)
@@ -27,4 +35,4 @@ const verify = nums => {
     return counts
 }
 
-console.log(verify(rand10(10000)))
+console.log(verify(createRand10List(10000)))
