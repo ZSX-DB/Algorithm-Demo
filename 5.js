105ccf2010ca7a5631d691e7fab27fc3df43b00c
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2021-06-20 02:53:47 +0800

    update

--- 5.js
+++ 5.js
@@ -4,54 +4,53 @@
  * 链接：https://leetcode-cn.com/problems/longest-palindromic-substring/
  */
 
-
-// function longestPalindrome(s) {
-//     let arr = [],
-//         sLen = s.length;
-//     //分割所有字符串
-//     for (let i = 0; i <= sLen; i++) {
-//         for (let j = 0; j <= sLen; j++) {
-//             arr.push(s.substring(i, j))
+// const longestPalindrome = s => {
+//     let len = s.length
+//     let res = ''
+//     let dp = Array.from(new Array(len), () => new Array(len).fill(0))
+//     for (let i = len - 1; i >= 0; i--) {
+//         for (let j = i; j < len; j++) {
+//             dp[i][j] = s[i] === s[j] && (j - i < 2 || dp[i + 1][j - 1])
+//             if (dp[i][j] && j - i + 1 > res.length) {
+//                 res = s.substring(i, j + 1)
+//             }
 //         }
 //     }
-//
-//     // return arr
-//
-//     let aLen = arr.length,
-//         duplicateArr = [];
-//     //获取回文字符串
-//     for (let k = 0; k < aLen; k++) {
-//         if (arr[k] === arr[k].split('').reverse().join('')) {
-//             duplicateArr.push(arr[k])
-//         }
-//     }
-//
-//     //输出最长的子串
-//     return duplicateArr.sort((a, b) => b.length - a.length)[0]
-//
+//     return res
 // }
 
-
-function longestPalindrome(s) {
-    let len=s.length,
-        res='',
-        dp=Array.from(new Array(len),()=>new Array(len).fill(0))
-
-    for(let i=len-1;i>=0;i--){
-        for(let j=i;j<len;j++){
-            dp[i][j]=s[i]===s[j]&&(j-i<2||dp[i+1][j-1])
-            if(dp[i][j]&&j-i+1>res.length){
-                res=s.substring(i,j+1)
-            }
+const longestPalindrome = s => {
+    const len = s.length
+    if (len === 1) return s
+    if (len === 2) return s[0] === s[1] ? s : s[0]
+    let longest = s[0]
+    for (let i = 1; i < len - 1; i++) {
+        let left = i - 1
+        let right = i + 1
+        let count = 1
+        while (s[left] && s[left] === s[i]) {
+            left--
         }
+        while (s[right] && s[right] === s[i]) {
+            right++
+        }
+        count += (right - left - 2)
+        while (s[left] && s[right] && s[left] === s[right]) {
+            left--
+            right++
+            count += 2
+        }
+        if (count > longest.length) longest = s.substring(left + 1, right)
     }
-
-    return res
-
+    return longest
 }
 
-
-
+console.log(longestPalindrome('abcbd'))
+console.log(longestPalindrome('abccbd'))
+console.log(longestPalindrome('abccccdd'))
+console.log(longestPalindrome('cbbd'))
 console.log(longestPalindrome('babad'))
+console.log(longestPalindrome('ccc'))
+console.log(longestPalindrome('aacabdkacaa'))
 
 
