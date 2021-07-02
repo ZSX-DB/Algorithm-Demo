6924d3afde4128540919eca931dacd3aedcb4f18
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2021-07-02 19:21:55 +0800

    update

--- 1833.js
+++ 1833.js
@@ -9,6 +9,8 @@ const maxIceCream = (costs, coins) => {
     return count
 }
 
+// s
+
 console.log(maxIceCream([1, 3, 2, 4, 1], 7))
 console.log(maxIceCream([10, 6, 8, 7, 7, 8], 5))
 console.log(maxIceCream([1, 6, 3, 1, 2, 5], 20))
