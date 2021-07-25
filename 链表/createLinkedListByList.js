175d55483d1e6a046a7569432a4287403bcfa228
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2021-07-25 13:37:29 +0800

    update

--- 链表/createLinkedListByList.js
+++ 链表/createLinkedListByList.js
@@ -1,3 +1,13 @@
 const createLinkedListByList = list => {
-
+    const head = {}
+    const lastIndex = list.length - 1
+    let cur = head
+    list.forEach((item, index) => {
+        cur.val = item
+        cur.next = index === lastIndex ? null : {}
+        cur = cur.next
+    })
+    return head
 }
+
+console.log(createLinkedListByList([1, 2, 3, 4, 5]))
