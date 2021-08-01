3a08f4cdfb1ea055dedf1a8ad6c35fb0305caab9
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2021-08-02 00:11:19 +0800

    update

--- 2.js
+++ 2.js
@@ -6,89 +6,100 @@
  * 链接：https://leetcode-cn.com/problems/add-two-numbers/
  */
 
-
-//这个方法基于数组
-// function addTwoNumbers(l1, l2) {
-//     let len1 = l1.length, len2 = l2.length, len = 0, temp = [];
-//     if (len1 >= len2) {
-//         len = len1;
-//         for (let i = 0; i < (len1 - len2); i++) {
-//             l2.unshift(0);
-//         }
-//     } else {
-//         len = len2;
-//         for (let i = 0; i < (len2 - len1); i++) {
-//             l1.unshift(0);
-//         }
-//     }
-
-//     for (let i = 0; i < len; i++) {
-//         temp[i] = l1[i] + l2[i];
-//     }
-//     while (len > 0) {
-//         if(temp[len-1]>=10){
-//             temp[len-1]=temp[len-1]-10;
-//             temp[len-2]=temp[len-2]+1;
-//         }
-//         len--;
-//     }
-//     return temp.join('').toString();
-// }
-
-// console.log(addTwoNumbers([2, 4, 3, 5], [5, 8, 4]));
-
-// function addTwoNumbers(l1,l2){
-//     let len=l1.length,temp=[];
-//     for(let i=0;i<len;i++){
-//         temp[i]=l1[i]+l2[i];
-//     }
-//     for(let i=0;i<len;i++){
-//         if(temp[i]>=10){
-//             temp[i]=temp[i]-10;
-//             temp[i+1]=temp[i+1]+1;
-//         }
-//     }
-//     return temp;
-// }
-
-// addTwoNumbers([2, 4, 3], [5, 6, 4])
-// console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]));
-
-// function sum(node) {
-//     let str='';
-//     str = String(node.val) + str;
-//     if(node.next != null) {
-//         str = sum(node.next, str);
-//     }
-//     return str;
-// }
-
-// console.log(sum(3,5,4))
-
-var addTwoNumbers = function (l1, l2) {
-    let p1 = l1
-    let p2 = l2
-    let carry = 0
-    const dummy = new ListNode()
-    let pointer = dummy
-    while (p1 || p2 || carry) {
-        const num1 = p1 ? p1.val : 0
-        const num2 = p2 ? p2.val : 0
-        const sum = num1 + num2 + carry
-        if (sum > 9) {
-            pointer.next = new ListNode(sum % 10)
-            carry = 1
-        } else {
-            pointer.next = new ListNode(sum)
-            carry = 0
-        }
-        if (p1) p1 = p1.next
-        if (p2) p2 = p2.next
-        pointer = pointer.next
+const addTwoNumbers = (l1, l2) => {
+    const nums = []
+    const root = {}
+    let num = 0
+    let cur = root
+    while (l1 || l2) {
+        const sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + num
+        nums.push(sum % 10)
+        num = Math.floor(sum / 10)
+        l1 = l1 ? l1.next : null
+        l2 = l2 ? l2.next : null
     }
-    return dummy.next
-};
+    if (num !== 0) {
+        nums.push(num)
+    }
+    while (nums.length) {
+        cur.val = nums.shift()
+        cur.next = nums.length ? {} : null
+        cur = cur.next
+    }
+    return root
+}
 
+// const addTwoNumbers = (l1, l2) => {
+//     let p1 = l1
+//     let p2 = l2
+//     let carry = 0
+//     const dummy = new ListNode()
+//     let pointer = dummy
+//     while (p1 || p2 || carry) {
+//         const num1 = p1 ? p1.val : 0
+//         const num2 = p2 ? p2.val : 0
+//         const sum = num1 + num2 + carry
+//         if (sum > 9) {
+//             pointer.next = new ListNode(sum % 10)
+//             carry = 1
+//         } else {
+//             pointer.next = new ListNode(sum)
+//             carry = 0
+//         }
+//         if (p1) p1 = p1.next
+//         if (p2) p2 = p2.next
+//         pointer = pointer.next
+//     }
+//     return dummy.next
+// }
 
-console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]));
+const l1 = {
+    val: 2,
+    next: {
+        val: 4,
+        next: {
+            val: 3,
+            next: null
+        }
+    }
+}
+
+const l2 = {
+    val: 5,
+    next: {
+        val: 6,
+        next: {
+            val: 4,
+            next: null
+        }
+    }
+}
+
+const l3 = {
+    val: 2,
+    next: {
+        val: 4,
+        next: {
+            val: 9,
+            next: null
+        }
+    }
+}
+
+const l4 = {
+    val: 5,
+    next: {
+        val: 6,
+        next: {
+            val: 4,
+            next: {
+                val: 9,
+                next: null
+            }
+        }
+    }
+}
+
+console.log(addTwoNumbers(l1, l2))
+console.log(addTwoNumbers(l3, l4))
 
