ca41dd11bc860b601f6624999aa9c24e50afcc99
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2021-08-10 01:36:10 +0800

    update

--- 7.js
+++ 7.js
@@ -5,23 +5,26 @@
  */
 
 // const reverse = x => {
-//     const val = String(Math.abs(x)).split('').reverse().join('')
-//     if (val > 2 ** 31) return 0
-//     return x > 0 ? val : -val
+//     let res = 0
+//     let num = Math.abs(x)
+//     while (num) {
+//         const remainder = num % 10
+//         res = res * 10 + remainder
+//         num = (num - remainder) / 10
+//     }
+//     return res > 2 ** 31 ? 0 : (x > 0 ? res : -res)
 // }
 
-
 const reverse = x => {
-    let res = 0
-    let num = Math.abs(x)
-    while (num) {
-        const remainder = num % 10
-        res = res * 10 + remainder
-        num = (num - remainder) / 10
+    if (x > 0) {
+        const y = Number(String(x).split('').reverse().join(''))
+        return y > 2 ** 31 - 1 ? 0 : y
+    } else {
+        const y = Number(String(-x).split('').reverse().join(''))
+        return -y < -(2 ** 31) ? 0 : -y
     }
-    return res > 2 ** 31 ? 0 : (x > 0 ? res : -res)
 }
 
 
-console.log(reverse(123))
+console.log(reverse(-123))
 console.log(reverse(1534236469))
