bd509a7df6bbc460924a7c35538c44b8bd8c50d6
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2021-08-22 02:01:43 +0800

    update

--- 15.js
+++ 15.js
@@ -4,49 +4,23 @@
  * 链接：https://leetcode-cn.com/problems/3sum
  */
 
-// 这部分代码可以解决普通情况，但对于[0,0,0,0,0]等特殊用例无法解决
-// function threeSum(nums) {
-//
-//     nums = nums.sort((a, b) => a - b)
-//
-//     // tdArr接收二维数组
-//     let len = nums.length,
-//         tdArr = []
-//
-//
-//     for (let i = 0; i < len; i++) {
-//         let left = i + 1,
-//             right = len - 1
-//
-//
-//         while (left<=right){
-//             if(nums[left]+nums[right]+nums[i]===0){
-//                 tdArr.push([nums[left],nums[right],nums[i]])
-//             }
-//             left++
-//             right--
-//         }
-//
-//     }
-//
-//     return tdArr
-//
-// }
-
-function threeSum(nums) {
-    let tdArr = [],
-        len = nums.length
-    if (len < 3) return tdArr
-    nums.sort((a, b) => a - b)
-    for(let i=0;i<len;i++){
-        if(nums[i]>0) break
-        if(i>0&&nums[i]===nums[i-1]) continue
-        let left=i+1,
-            right=len-1
+const threeSum = nums => {
+    nums.sort((x, y) => x - y)
+    const len = nums.length
+    const result = []
+    for (let i = 0; i < len; i++) {
+        if (nums[i] > 0) {
+            break
+        }
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue
+        }
+        let left = i + 1
+        let right = len - 1
         while (left < right) {
-            let sum = nums[i] + nums[left] + nums[right]
+            const sum = nums[i] + nums[left] + nums[right]
             if (sum === 0) {
-                tdArr.push([nums[i], nums[left], nums[right]])
+                result.push([nums[i], nums[left], nums[right]])
                 while (left < right && nums[left] === nums[left + 1]) {
                     left++
                 }
@@ -55,22 +29,22 @@ function threeSum(nums) {
                 }
                 left++
                 right--
+            } else if (sum > 0) {
+                right--
             } else if (sum < 0) {
                 left++
-            } else {
-                right--
             }
         }
     }
-    return tdArr
+    return result
 }
-
 
 
 console.log(threeSum([]))
 console.log(threeSum([-1]))
 console.log(threeSum([-1, 0]))
-
 console.log(threeSum([-1, 0, 1, 2, -1, -4]))
 console.log(threeSum([0, 0, 0]))
 console.log(threeSum([0, 0, 0, 0, 0, 0, 0, 1]))
+console.log(threeSum([-2, 0, 1, 1, 2]))
+console.log(threeSum([34, 55, 79, 28, 46, 33, 2, 48, 31, -3, 84, 71, 52, -3, 93, 15, 21, -43, 57, -6, 86, 56, 94, 74, 83, -14, 28, -66, 46, -49, 62, -11, 43, 65, 77, 12, 47, 61, 26, 1, 13, 29, 55, -82, 76, 26, 15, -29, 36, -29, 10, -70, 69, 17, 49]))
