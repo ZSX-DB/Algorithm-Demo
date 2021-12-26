f3aecae819d981aa05d61b191339c13855fb8e86
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2021-12-26 23:52:21 +0800

    update: 17.ts

--- typescript_version/17.ts
+++ typescript_version/17.ts
@@ -3,15 +3,8 @@ const letterCombinations = (digits: string): string[] => {
         ['', []], ['2', ['a', 'b', 'c']], ['3', ['d', 'e', 'f']], ['4', ['g', 'h', 'i']], ['5', ['j', 'k', 'l']],
         ['6', ['m', 'n', 'o']], ['7', ['p', 'q', 'r', 's']], ['8', ['t', 'u', 'v']], ['9', ['w', 'x', 'y', 'z']]
     ])
-    const getStrIN = (strList1: string[], strList2: string[]): string[] => {
-        const result: string[] = []
-        for (const s1 of strList1) {
-            for (const s2 of strList2) {
-                result.push(s1 + s2)
-            }
-        }
-        return result
-    }
+    const getStrIN = (strList1: string[], strList2: string[]): string[] =>
+        strList1.reduce<string[]>((result, s1) => [...result, ...strList2.map(s2 => s1 + s2)], [])
     return digits.length <= 1 ? record.get(digits) : getStrIN(record.get(digits[0]), letterCombinations(digits.substring(1)))
 }
 
