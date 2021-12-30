291187020bad1c7c1445ce6ef6cfea34700df44f
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2021-12-31 00:57:42 +0800

    update: 18

--- 18.js
+++ 18.js
@@ -4,46 +4,52 @@
  * 链接：https://leetcode-cn.com/problems/4sum/
  */
 
-function fourSum(nums, target) {
-    const list = []
-    const len = nums.length
-    if (len < 4) return list
-    nums.sort((a, b) => a - b)
-    for (let i = 0; i < len - 3; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue
-        // 如果最小的四个值都超过target,则无任何符合情况
-        if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break
-        // 当前值与最大的三个值相加小于target, 则枚举到i+1
-        if (nums[i] + nums[len - 1] + nums[len - 2] + nums[len - 3] < target) continue
-        for (let j = i + 1; j < len - 2; j++) {
-            // 与上方同理
-            if (j > i + 1 && nums[j] === nums[j - 1]) continue
-            if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break
-            if (nums[i] + nums[j] + nums[len - 1] + nums[len - 2] < target) continue
-            // 双指针取值
-            let left = j + 1
-            let right = len - 1
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+const fourSum = (nums, target) => {
+    const threeSum = (nums, target) => {
+        const length = nums.length
+        const result = []
+        for (let i = 0; i < length; i++) {
+            if (nums[i] === nums[i - 1]) {
+                continue
+            }
+            let left = i + 1
+            let right = length - 1
             while (left < right) {
-                const sum = nums[i] + nums[j] + nums[left] + nums[right]
-                if (sum === target) {
-                    list.push([nums[i], nums[j], nums[left], nums[right]])
+                const sum = nums[i] + nums[left] + nums[right]
+                if (sum > target) {
+                    right--
+                } else if (sum < target) {
+                    left++
+                } else {
                     while (left < right && nums[left] === nums[left + 1]) {
                         left++
                     }
-                    left++
-                    while (left < right && nums[right] === nums[right + 1]) {
+                    while (left < right && nums[right] === nums[right - 1]) {
                         right--
                     }
-                    right--
-                } else if (sum > target) {
-                    right--
-                } else {
+                    result.push([nums[i], nums[left], nums[right]])
                     left++
+                    right--
                 }
             }
         }
+        return result
     }
-    return list
+    const record = new Set()
+    nums.sort((x, y) => x - y)
+    for (let i = 0; i < nums.length; i++) {
+        threeSum(nums.slice(i + 1), target - nums[i])
+            .map(item => [...item, nums[i]].sort((x, y) => x - y))
+            .forEach(item => {
+                record.add(JSON.stringify(item))
+            })
+    }
+    return [...record].map(item => JSON.parse(item))
 }
 
 console.log(fourSum([1, 0, -1, 0, -2, 2], 0))
