e146a47947041a678df230ab6801b860c9505f9e
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2022-01-08 22:10:31 +0800

    update: 22.ts

--- 22.js
+++ 22.js
@@ -4,83 +4,19 @@
  * 链接：https://leetcode-cn.com/problems/generate-parentheses/
  */
 
-
-// 通过获得所有情况再去除不符合情况的，时间复杂度和空间复杂度均不好
-// function generateParenthesis(n) {
-//     let arr=[],
-//         end=2*n-1
-//
-//     if(n===0)return []
-//
-//     const getItem=(start,str)=> {
-//         if(start===end){
-//             arr.push(str)
-//             return arr
-//         }
-//
-//         getItem(start+1,str+'(')
-//         getItem(start+1,str+')')
-//
-//     }
-//
-//     getItem(0,'(')
-//
-//     let arrLen=arr.length,
-//         itemLen=end+1,
-//         temp=0,
-//         newArr=[]
-//
-//     while (temp<arrLen){
-//         let tempArr=[]
-//         for(let i=0;i<itemLen;i++){
-//
-//             if(arr[temp][i]===')'&&tempArr[tempArr.length-1]==='('){
-//                 tempArr.pop()
-//             }else {
-//                 tempArr.push(arr[temp][i])
-//             }
-//
-//         }
-//         if(tempArr.length===0){
-//             newArr.push(arr[temp])
-//         }
-//
-//         temp++
-//     }
-//
-//     return newArr
-// }
-
-
-// 深度优先算法
-// 添加需满足2个条件
-// 1、左括号 < n
-// 2、添加右括号时，左括号数量必须大于右括号数量，并且右括号 < n
-function generateParenthesis(n) {
-    let arr=[]
-
-    const create=(left,right,str)=> {
-        if(left===n&&right===n){
-            arr.push(str)
-            return arr
-        }
-
-        if(left<n){
-            create(left+1,right,str+'(')
-        }
-        if(right<left){
-            create(left,right+1,str+')')
-        }
-
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+const generateParenthesis = n => n === 1 ? ['()'] : [...new Set(generateParenthesis(n - 1).map(item => {
+    const record = []
+    for (let i = 0; i <= item.length; i++) {
+        record.push(`${item.substring(0, i)}()${item.substring(i, item.length)}`)
     }
-
-    create(0,0,'')
-
-    return arr
-
-}
+    return record
+}).flat(1))]
 
 
 console.log(generateParenthesis(3))
-console.log(generateParenthesis(0))
+console.log(generateParenthesis(1))
 
