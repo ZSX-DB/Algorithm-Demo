33a3911ce672f4e92079e56a55fcc17e081bae8b
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2022-08-18 14:35:50 +0800

    update

--- javascript_version/1512.js
+++
@@ -1,8 +0,0 @@
-const numIdenticalPairs = nums =>
-    [...nums.reduce((records, num, idx) => records.set(num, records.has(num) ? [...records.get(num), idx] : [idx]), new Map()).values()]
-        .map(record => (record.length * (record.length - 1) / 2))
-        .reduce((sum, num) => sum + num, 0)
-
-
-console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3]))
-console.log(numIdenticalPairs([1, 1, 1, 1]))
